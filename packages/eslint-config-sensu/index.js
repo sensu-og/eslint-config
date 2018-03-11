df17c762d71e3f41f58233593a27677ae5ce8530
James Phillips <jamesdphillips@gmail.com>
2018-03-10 19:44:38 -0800

    [Core] Add filenames plugin
    Signed-off-by: James Phillips <jamesdphillips@gmail.com>

--- packages/eslint-config-sensu/index.js
+++ packages/eslint-config-sensu/index.js
@@ -7,6 +7,7 @@ module.exports = {
     "./rules/strict",
     "./rules/modern",
     "./rules/imports",
+    "./rules/filenames",
     "./rules/style",
   ].map(require.resolve),
   parser: "babel-eslint",
